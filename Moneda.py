40f221a7b0f4718bc082020fe68be403a14ef9f6
Lucas Orsi <lorsi@skyloom.co>
2021-09-11 21:04:20 -0300

    Adds Moneda documentation

--- Moneda.py
+++ Moneda.py
@@ -1,4 +1,6 @@
+
 class Moneda:
+    '''Representa un tipo de cambio, con su nombre y valores de compraventa'''
     def __init__(self, id='0', name='?', val_compra=0, val_venta=0) -> None:
         self._id = id
         self._name = name
@@ -6,14 +8,16 @@ class Moneda:
         self._val_venta = val_venta
 
     @classmethod
-    def from_csv_entry(cls, val:str):
+    def from_csv_entry(cls, val:str) -> object:
+        '''Construye una instancia de Moneda a partir de valores separados por coma'''
         try:
             id, name, val_compra, val_venta = val.split(',')
             return cls(int(id), name, float(val_compra) ,float(val_venta))
         except ValueError as e:
             raise ValueError('Error al parsear entrada de CSV de moneda: ({})'.format(val.replace("\n", "")))
 
-    def as_dict(self):
+    def as_dict(self) -> dict:
+        '''Obtiene una representación de forma de diccionario de la moneda'''
         return {
             'id':self._id,
             'value1':self._val_compra,
@@ -22,6 +26,7 @@ class Moneda:
         }
 
     def serialize_csv(self):
+        '''Serializa la moneda en una entrada separada por comas'''
         return f'{self._id},{self._name},{self._val_compra},{self._val_venta}'
 
     def __eq__(self, o: object) -> bool:
